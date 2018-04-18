2d2515289fb761614f0323075f8d8962c134aad2
djmz428 <37299732+djmz428@users.noreply.github.com>
2018-04-18 10:29:44 -0400

    Progression
    Completed the majority of the jQuery functionality prior to styling updates 4/18/18 10:29

--- To Do List/assets/js/toDo.js
+++ To Do List/assets/js/toDo.js
@@ -1,12 +1,23 @@
 //check off ToDos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete To-Do
-$("span").click(function(event) {
-	$(this).parent().fadeOut(function() {
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//add new To-Do when 'enter' is pressed
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		//get todo from input, then clear it
+		var todoText = ($(this).val());
+		$(this).val("");
+		//create new li and add it to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
